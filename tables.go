437e4f071e81daafe7f21d312b8d2ba2190d2f25
qmuntal <qmuntaldiaz@microsoft.com>
2022-08-26 14:54:53 +0200

    update tables with new genlayout spec

--- tables.go
+++ tables.go
@@ -1,37 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-
+//
+//go:generate genlayout
 package winmd
 
-import (
-	"github.com/microsoft/go-winmd/flags"
-)
-
-// Record is an item in a metadata table.
-type Record = any
-
-// Index indexes a record of type T.
-type Index[T Record] int
-
-// CodedIndex indexes a record on any table.
-type CodedIndex struct {
-	Index[any]
-	Table int
-}
-
-// Slice indexes the range of records [Start,End) on the table T.
-type Slice[T Record] struct {
-	Start Index[T]
-	End   Index[T]
-}
-
-// BlobIndex is an index to a #Blob heap.
-type BlobIndex int
-
-// GuidIndex is an index to a #GUID heap.
-type GUIDIndex int
+import "github.com/microsoft/go-winmd/flags"
 
 // Assembly is defined in §II.22.2.
+// @table=0x20
 type Assembly struct {
 	HashAlgID      flags.AssemblyHashAlgorithm
 	MajorVersion   uint16
@@ -47,6 +23,7 @@ type Assembly struct {
 // assemblyOS is defined in §II.22.3.
 // This record should not be emitted into any PE file,
 // but needed temporarily to calculate sizes and offsets for subsequent tables.
+// @table=0x22
 type assemblyOS struct {
 	OSPlatformID   uint32
 	OSMajorVersion uint32
@@ -56,11 +33,13 @@ type assemblyOS struct {
 // assemblyProcessor is defined in §II.22.4.
 // This record should not be emitted into any PE file,
 // but needed temporarily to calculate sizes and offsets for subsequent tables.
+// @table=0x21
 type assemblyProcessor struct {
 	Processor uint32
 }
 
 // AssemblyRef is defined in §II.22.5.
+// @table=0x23
 type AssemblyRef struct {
 	MajorVersion     uint16
 	MinorVersion     uint16
@@ -76,73 +55,83 @@ type AssemblyRef struct {
 // assemblyRefOS is defined in §II.22.6.
 // This record should not be emitted into any PE file,
 // but needed temporarily to calculate sizes and offsets for subsequent tables.
+// @table=0x25
 type assemblyRefOS struct {
 	OSPlatformID   uint32
 	OSMajorVersion uint32
 	OSMinorVersion uint32
-	AssemblyRef    Index[AssemblyRef]
+	AssemblyRef    Index // @ref=AssemblyRef
 }
 
 // assemblyRefProcessor is defined in §II.22.7.
 // This record should not be emitted into any PE file,
 // but needed temporarily to calculate sizes and offsets for subsequent tables.
+// @table=0x24
 type assemblyRefProcessor struct {
 	Processor   uint32
-	AssemblyRef Index[AssemblyRef]
+	AssemblyRef Index // @ref=AssemblyRef
 }
 
 // ClassLayout is defined in §II.22.8.
+// @table=0x0F
 type ClassLayout struct {
 	PackingSize uint16
 	ClassSize   uint32
-	Parent      Index[TypeDef]
+	Parent      Index // @ref=TypeDef
 }
 
 // Constant is defined in §II.22.9.
+// @table=0x0B
 type Constant struct {
 	Type    flags.ElementType
 	Padding byte       // 1-byte padding zero
-	Parent  CodedIndex `winmd:"HasConstant"`
+	Parent  CodedIndex // @code=HasConstant
 	Value   BlobIndex
 }
 
 // CustomAttribute is defined in §II.22.10.
+// @table=0x0C
 type CustomAttribute struct {
-	Parent CodedIndex `winmd:"HasCustomAttribute"`
-	Type   CodedIndex `winmd:"CustomAttributeType"`
+	Parent CodedIndex // @code=HasCustomAttribute
+	Type   CodedIndex // @code=CustomAttributeType
 	Value  BlobIndex
 }
 
 // DeclSecurity is defined in §II.22.11.
+// @table=0x0E
 type DeclSecurity struct {
 	Action        uint16
-	Parent        CodedIndex `winmd:"HasDeclSecurity"`
+	Parent        CodedIndex // @code=HasDeclSecurity
 	PermissionSet BlobIndex
 }
 
 // EventMap is defined in §II.22.12.
+// @table=0x12
 type EventMap struct {
-	Parent    Index[TypeDef]
-	EventList Slice[Event]
+	Parent    Index // @ref=TypeDef
+	EventList Slice // @ref=Event
 }
 
 // Event is defined in §II.22.13.
+// @table=0x14
 type Event struct {
 	EventFlags flags.EventAttributes
 	Name       string
-	EventType  CodedIndex `winmd:"TypeDefOrRef"`
+	EventType  CodedIndex // @code=TypeDefOrRef
 }
 
 // ExportedType is defined in §II.22.14.
+// @table=0x27
 type ExportedType struct {
 	Flags          flags.TypeAttributes
 	TypeDefID      uint32 // index into a TypeDef table, used as hint only
 	Name           string
-	Namespace      string     `winmd:",cache"`
-	Implementation CodedIndex `winmd:"Implementation"`
+	Namespace      string
+	Implementation CodedIndex // @code=Implementation
 }
 
 // Field is defined in §II.22.15.
+// @table=0x04
 type Field struct {
 	Flags     flags.FieldAttributes
 	Name      string
@@ -150,24 +139,28 @@ type Field struct {
 }
 
 // FieldLayout is defined in §II.22.16.
+// @table=0x10
 type FieldLayout struct {
 	Offset uint32
-	Field  Index[Field]
+	Field  Index // @ref=Field
 }
 
 // FieldMarshal is defined in §II.22.17.
+// @table=0x0D
 type FieldMarshal struct {
-	Parent     CodedIndex `winmd:"HasFieldMarshal"`
+	Parent     CodedIndex // @code=HasFieldMarshal
 	NativeType BlobIndex
 }
 
 // FieldRVA is defined in §II.22.18.
+// @table=0x1D
 type FieldRVA struct {
 	RVA   uint32
-	Field Index[Field]
+	Field Index // @ref=Field
 }
 
 // File is defined in §II.22.19.
+// @table=0x26
 type File struct {
 	Flags     flags.FileAttributes
 	Name      string
@@ -175,79 +168,90 @@ type File struct {
 }
 
 // GenericParam is defined in §II.22.20.
+// @table=0x2A
 type GenericParam struct {
 	Number uint16
 	Flags  flags.GenericParamAttributes
-	Owner  CodedIndex `winmd:"TypeOrMethodDef"`
+	Owner  CodedIndex // @code=TypeOrMethodDef
 	Name   string
 }
 
 // GenericParam is defined in §II.22.21.
+// @table=0x2C
 type GenericParamConstraint struct {
-	Owner      Index[GenericParam]
-	Constraint CodedIndex `winmd:"TypeDefOrRef"`
+	Owner      Index      // @ref=GenericParam
+	Constraint CodedIndex // @code=TypeDefOrRef
 }
 
 // ImplMap is defined in §II.22.22.
+// @table=0x1C
 type ImplMap struct {
 	MappingFlags    flags.PInvokeAttributes
-	MemberForwarded CodedIndex `winmd:"MemberForwarded"`
+	MemberForwarded CodedIndex // @code=MemberForwarded
 	ImportName      string
-	ImportScope     Index[ModuleRef]
+	ImportScope     Index // @ref=ModuleRef
 }
 
 // InterfaceImpl is defined in §II.22.23.
+// @table=0x09
 type InterfaceImpl struct {
-	Class     Index[TypeDef]
-	Interface CodedIndex `winmd:"TypeDefOrRef"`
+	Class     Index      // @ref=TypeDef
+	Interface CodedIndex // @code=TypeDefOrRef
 }
 
 // ManifestResource is defined in §II.22.24.
+// @table=0x28
 type ManifestResource struct {
 	Offset         uint32
 	Flags          flags.ManifestResourceAttributes
 	Name           string
-	Implementation CodedIndex `winmd:"Implementation"`
+	Implementation CodedIndex // @code=Implementation
 }
 
 // MemberRef is defined in §II.22.25.
+// @table=0x0A
 type MemberRef struct {
-	Class     CodedIndex `winmd:"MemberRefParent"`
+	Class     CodedIndex // @code=MemberRefParent
 	Name      string
 	Signature BlobIndex
 }
 
 // MethodDef is defined in §II.22.26.
+// @table=0x06
 type MethodDef struct {
 	RVA       uint32
 	ImplFlags flags.MethodImplAttributes
 	Flags     flags.MethodAttributes
 	Name      string
 	Signature BlobIndex
-	ParamList Slice[Param]
+	ParamList Slice // @ref=Param
 }
 
 // MethodImpl is defined in §II.22.27.
+// @table=0x19
 type MethodImpl struct {
-	Class             Index[TypeDef]
-	MethodBody        CodedIndex `winmd:"MethodDefOrRef"`
-	MethodDeclaration CodedIndex `winmd:"MethodDefOrRef"`
+	Class             Index      // @ref=TypeDef
+	MethodBody        CodedIndex // @code=MethodDefOrRef
+	MethodDeclaration CodedIndex // @code=MethodDefOrRef
 }
 
 // MethodImpl is defined in §II.22.28.
+// @table=0x18
 type MethodSemantics struct {
 	Semantics   flags.MethodSemanticsAttributes
-	Method      Index[MethodDef]
-	Association CodedIndex `winmd:"HasSemantics"`
+	Method      Index      // @ref=MethodDef
+	Association CodedIndex // @code=HasSemantics
 }
 
 // MethodSpec is defined in §II.22.29.
+// @table=0x2B
 type MethodSpec struct {
-	Method        CodedIndex `winmd:"MethodDefOrRef"`
+	Method        CodedIndex // @code=MethodDefOrRef
 	Instantiation BlobIndex
 }
 
 // Module is defined in §II.22.30.
+// @table=0x00
 type Module struct {
 	Generation uint16
 	Name       string
@@ -257,24 +261,28 @@ type Module struct {
 }
 
 // ModuleRef is defined in §II.22.31.
+// @table=0x1A
 type ModuleRef struct {
 	Name string
 }
 
 // NestedClass is defined in §II.22.32.
+// @table=0x29
 type NestedClass struct {
-	NestedClass    Index[TypeDef]
-	EnclosingClass Index[TypeDef]
+	NestedClass    Index // @ref=TypeDef
+	EnclosingClass Index // @ref=TypeDef
 }
 
 // Param is defined in §II.22.33.
+// @table=0x08
 type Param struct {
 	Flags    flags.ParamAttributes
 	Sequence uint16
-	Name     string // String heap
+	Name     string
 }
 
 // Property is defined in §II.22.34.
+// @table=0x17
 type Property struct {
 	Flags flags.PropertyAttributes
 	Name  string
@@ -282,34 +290,39 @@ type Property struct {
 }
 
 // PropertyMap is defined in §II.22.35.
+// @table=0x15
 type PropertyMap struct {
-	Parent       Index[TypeDef]
-	PropertyList Slice[Property]
+	Parent       Index // @ref=TypeDef
+	PropertyList Slice // @ref=Property
 }
 
 // StandAloneSig is defined in §II.22.36.
+// @table=0x11
 type StandAloneSig struct {
 	Signature BlobIndex
 }
 
 // TypeDef is defined in §II.22.37.
+// @table=0x02
 type TypeDef struct {
 	Flags      flags.TypeAttributes
 	Name       string
-	Namespace  string     `winmd:",cache"`
-	Extends    CodedIndex `winmd:"TypeDefOrRef"`
-	FieldList  Slice[Field]
-	MethodList Slice[MethodDef]
+	Namespace  string
+	Extends    CodedIndex // @code=TypeDefOrRef
+	FieldList  Index      // @ref=Field
+	MethodList Index      // @ref=MethodDef
 }
 
 // TypeRef is defined in §II.22.38.
+// @table=0x01
 type TypeRef struct {
-	ResolutionScope CodedIndex `winmd:"ResolutionScope"`
+	ResolutionScope CodedIndex // @code=ResolutionScope
 	Name            string
-	Namespace       string `winmd:",cache"`
+	Namespace       string
 }
 
 // TypeSpec is defined in §II.22.39.
+// @table=0x1B
 type TypeSpec struct {
 	Signature BlobIndex
 }
---
+++ winmd.go
@@ -0,0 +1,123 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package winmd
+
+import (
+	"bytes"
+	"debug/pe"
+	"fmt"
+	"io"
+)
+
+// A Metadata represents an open Windows Metadata file.
+type Metadata struct {
+	Version string
+	Tables  *Tables
+	Strings StringHeap
+	US      USHeap
+	Blob    BlobHeap
+	GUID    GUIDHeap
+}
+
+// New creates a new File from an underlying PE file.
+func New(pefile *pe.File) (*Metadata, error) {
+	return newMetadata(pefile)
+}
+
+// Tables provides access to the tables and records stored in the #~ stream
+// as defined in §II.24.2.6
+type Tables struct {
+	tables
+	data    []byte
+	strings StringHeap
+	layout  layout
+}
+
+// GUIDHeap provides access to the #GUID heap as defined in §II.24.2.5.
+type GUIDHeap []byte
+
+// USHeap provides access to the #US heap as defined in §II.24.2.4.
+type USHeap []byte
+
+// BlobHeap provides access to the #Blob heap as defined in §II.24.2.4.
+type BlobHeap []byte
+
+// StringHeap provides access to #Strings heap as defined in §II.24.2.3.
+type StringHeap []byte
+
+// String extracts string from the string heap st at offset start.
+func (sh StringHeap) String(start uint32) (string, error) {
+	if int(start) >= len(sh) {
+		return "", fmt.Errorf("offset %d is beyond the end of string heap", start)
+	}
+	end := bytes.IndexByte(sh[start:], '\x00')
+	if end == -1 {
+		return "", fmt.Errorf("offset %d is not null-terminated", start)
+	}
+	return string(sh[start : int(start)+end]), nil
+}
+
+// Record is an item in a metadata table
+type Record interface {
+	table() table
+}
+
+// Index indexes a record in a table.
+type Index uint32
+
+// CodedIndex indexes a record on any table.
+type CodedIndex struct {
+	Index Index
+	table table
+}
+
+// IsNull returns true if the coded index points to a null record.
+func (c CodedIndex) IsNull() bool {
+	return c.table != tableNone
+}
+
+// BlobIndex is an index to a #Blob heap.
+type BlobIndex uint32
+
+// GuidIndex is an index to a #GUID heap.
+type GUIDIndex uint32
+
+// Slice indexes the range of records [Start,End) on the table T.
+type Slice struct {
+	Start Index
+	End   Index
+}
+
+// Table is a record container as defined in §II.22.
+type Table[T Record] struct {
+	Len    uint32
+	tables *Tables
+}
+
+func newTable[T Record](t *Tables, table table) Table[T] {
+	return Table[T]{t.layout.tables[table].rows, t}
+}
+
+// Record returns the record at row.
+func (t Table[T]) Record(row Index) (*T, error) {
+	if uint32(row) >= t.Len {
+		return nil, fmt.Errorf("row %d is beyond the end of the table", row)
+	}
+	var rec T
+	info := t.tables.layout.tables[rec.table()]
+	offset := info.rowOffset(uint32(row))
+	if offset+int(info.width) >= len(t.tables.data) {
+		return nil, io.ErrUnexpectedEOF
+	}
+	// instantiate and decode the record
+	r := recordReader{
+		data: t.tables.data[offset : offset+(int(info.width)*int(info.rows-uint32(row)))], strings: t.tables.strings,
+		layout: &t.tables.layout,
+	}
+	err := (any)(&rec).(interface{ decode(r recordReader) error }).decode(r)
+	if err != nil {
+		return nil, err
+	}
+	return &rec, err
+}
