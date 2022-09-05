c5c85540444c0f18f36b75948a295777264c64eb
qmuntal <qmuntaldiaz@microsoft.com>
2022-09-05 16:24:56 +0200

    add tables test

--- file_test.go
+++ file_test.go
@@ -40,4 +40,29 @@ func TestNew(t *testing.T) {
 	testHeap(f.US, 4)
 	testHeap(f.GUID, 16)
 	testHeap(f.Blob, 1169700)
+
+	testLen(t, f.Tables.Module, 1)
+	testLen(t, f.Tables.TypeRef, 14595)
+	testLen(t, f.Tables.TypeDef, 33749)
+	testLen(t, f.Tables.Field, 225577)
+	testLen(t, f.Tables.MethodDef, 66944)
+	testLen(t, f.Tables.Param, 206634)
+	testLen(t, f.Tables.ClassLayout, 1046)
+	testLen(t, f.Tables.Assembly, 1)
+	testLen(t, f.Tables.AssemblyRef, 4)
+	testLen(t, f.Tables.InterfaceImpl, 7550)
+	testLen(t, f.Tables.MemberRef, 24)
+	testLen(t, f.Tables.Constant, 144249)
+	testLen(t, f.Tables.CustomAttribute, 74268)
+	testLen(t, f.Tables.FieldLayout, 3860)
+	testLen(t, f.Tables.ModuleRef, 344)
+	testLen(t, f.Tables.ImplMap, 17148)
+	testLen(t, f.Tables.NestedClass, 1744)
+}
+
+func testLen[T winmd.Record](t *testing.T, table winmd.Table[T], size uint32) {
+	t.Helper()
+	if table.Len != size {
+		t.Errorf("len = %v, want %v", table.Len, size)
+	}
 }
